0ce60844b92a6bc0032f8cef10485661ee407110
serkan serdar <serkanserdar61@gmail.com>
2019-11-12 13:16:22 -0500

    myproject

--- src/test/java/Tests/day7/CssselectorPractice.java
+++ src/test/java/Tests/day7/CssselectorPractice.java
@@ -16,10 +16,18 @@ public class CssselectorPractice {
         //.classname  but if there is some space in class name then put .(origin class name is btn btn-primary)
         // or we can use [class='btn btn-primary']
         List<WebElement> buttons=driver.findElements(By.cssSelector(".btn.btn-primary"));
-        for(WebElement each:buttons){
+        for(WebElement each:buttons) {
             each.click();
             BrowserUtils.wait(1);
+            WebElement message = driver.findElement(By.cssSelector("#result"));
+            System.out.println(message.getText());
+        }
+            WebElement header=driver.findElement(By.cssSelector(".container>h3"));
+            System.out.println(header.getText());
+            WebElement p=driver.findElement(By.cssSelector("[class='container']"));
+            System.out.println(p.getText());
+        }
         }
 
-    }
-}
+
+
